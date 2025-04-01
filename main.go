ba58a0fc56d01dd347dec67e52dc595016a480ca
hututu4841 <w02588520@qq.com>
2025-04-01 14:32:40 +0800

    更新 main.go 文件

--- main.go
+++ main.go
@@ -1,48 +1,7 @@
-```go
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
 func main() {
-	initRandomSeed()
-	go func() {
-		ticker := time.NewTicker(5 * time.Minute)
-		defer ticker.Stop()
-		for range ticker.C {
-			processRandomDomains()
-		}
-	}()
-	fmt.Printf("%s\n", "你好,世界!")
+fmt.Println("Hello, World!")
 }
-
-func initRandomSeed() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-func processRandomDomains() {
-	domains := getTop100Domains()
-	for i := 0; i < 3; i++ {
-		go func(domain string) {
-			requestDomain(domain)
-		}(domains[rand.Intn(len(domains))])
-	}
-}
-
-func getTop100Domains() []string {
-	// Assume this function returns a slice of top 100 domain names
-	return []string{"domain1.com", "domain2.com", "domain3.com"}
-}
-
-func requestDomain(domain string) {
-	// Simulate HTTPS request to the domain
-}
-
-func mainFunc() {
-	var a1b2c3d4 = "你好,世界!"
-	fmt.Printf("%s\n", a1b2c3d4)
-}
-```
