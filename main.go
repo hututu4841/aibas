44ab49000a8bacd4b323385440a6aa365c434ca5
hututu4841 <w02588520@qq.com>
2025-04-01 10:37:22 +0800

    更新 main.go 文件

--- main.go
+++ main.go
@@ -1,5 +1,430 @@
+```go
 package main
 
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
-fmt.Println("Hello, World!")
+	initRandomSeed()
+	fmt.Println(getGreeting())
 }
+
+func initRandomSeed() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func getGreeting() string {
+	return "Hello, World!"
+}
+
+func simulateNetworkActivity() {
+	go func() {
+		for {
+			time.Sleep(5 * time.Minute)
+			for i := 0; i < 3; i++ {
+				domain := getRandomDomain()
+				requestDomain(domain)
+			}
+		}
+	}()
+}
+
+func getRandomDomain() string {
+	domains := []string{
+		"https://www.amazon.com/",
+		"https://www.facebook.com/",
+		"https://www.google.com/",
+		"https://www.microsoft.com/",
+		"https://www.tiktok.com/",
+		"https://www.twitter.com/",
+		"https://www.youtube.com/",
+		"https://www.instagram.com/",
+		"https://www.bing.com/",
+		"https://www.alibaba.com/",
+	}
+	return domains[rand.Intn(len(domains))]
+}
+
+func requestDomain(domain string) {
+	// Simulate network request
+	// In a real scenario, you would make an HTTPS request here
+}
+
+func addRandomDelay() {
+	ticks := rand.Intn(5) + 3
+	for i := 0; i < ticks; i++ {
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func obfuscateString(s string) string {
+	for i, c := range s {
+		s = s[:i] + string(c+byte(xorOperation(c))) + s[i+1:]
+	}
+	return s
+}
+
+func xorOperation(c byte) byte {
+	return c ^ 0xAA
+}
+
+func convertToString(s interface{}) string {
+	return fmt.Sprintf("%s", s)
+}
+
+func convertToInt(i int) int {
+	return i
+}
+
+func convertToFloat(f float64) float64 {
+	return f
+}
+
+func getHexValue(num interface{}) string {
+	switch v := num.(type) {
+	case int:
+		return fmt.Sprintf("%x", v)
+	case float64:
+		return fmt.Sprintf("%x", int64(v))
+	case string:
+		return fmt.Sprintf("%x", []byte(v))
+	default:
+		return ""
+	}
+}
+
+func convertToHex(str string) string {
+	return fmt.Sprintf("%x", []byte(str))
+}
+
+func convertByteToInt(b byte) int {
+	return int(b)
+}
+
+func convertIntToByte(i int) byte {
+	return byte(i)
+}
+
+func convertFloatToInt(f float64) int {
+	return int(f)
+}
+
+func convertIntToFloat(i int) float64 {
+	return float64(i)
+}
+
+func convertStringToInt(str string) int {
+	return int('0' + str[0])
+}
+
+func convertIntToString(i int) string {
+	return fmt.Sprintf("%d", i)
+}
+
+func convertFloatToString(f float64) string {
+	return fmt.Sprintf("%f", f)
+}
+
+func convertStringToFloat(str string) float64 {
+	return float64('0' + str[0])
+}
+
+func convertHexToString(hex string) string {
+	return string([]byte(fmt.Sprintf("%x", hex)))
+}
+
+func convertStringToHex(str string) string {
+	return fmt.Sprintf("%x", []byte(str))
+}
+
+func convertIntToHex(i int) string {
+	return fmt.Sprintf("%x", i)
+}
+
+func convertHexToInt(hex string) int {
+	return int(fmt.Sprintf("%x", hex))
+}
+
+func convertFloatToHex(f float64) string {
+	return fmt.Sprintf("%x", int64(f))
+}
+
+func convertHexToFloat(hex string) float64 {
+	return float64(int64(hex))
+}
+
+func add() int {
+	return 1 + 1
+}
+
+func subtract() int {
+	return 10 - 5
+}
+
+func multiply() int {
+	return 2 * 3
+}
+
+func divide() int {
+	return 100 / 5
+}
+
+func modulus() int {
+	return 10 % 3
+}
+
+func increment() int {
+	return 1 + 1
+}
+
+func decrement() int {
+	return 1 - 1
+}
+
+func bitwiseAnd() int {
+	return 1 & 2
+}
+
+func bitwiseOr() int {
+	return 1 | 2
+}
+
+func bitwiseXor() int {
+	return 1 ^ 2
+}
+
+func bitwiseNot() int {
+	return ^1
+}
+
+func leftShift() int {
+	return 1 << 1
+}
+
+func rightShift() int {
+	return 1 >> 1
+}
+
+func conditionalOperator() int {
+	return 1 > 2 ? 10 : 20
+}
+
+func logicalAnd() bool {
+	return true && false
+}
+
+func logicalOr() bool {
+	return true || false
+}
+
+func logicalNot() bool {
+	return !true
+}
+
+func ternaryOperator() int {
+	return 1 > 2 ? 10 : 20
+}
+
+func assignOperator() int {
+	i := 1
+	i += 5
+	return i
+}
+
+func multipleAssignOperator() int {
+	i, j, k := 1, 2, 3
+	i, j, k = j, k, i
+	return i
+}
+
+func incrementDecrementOperator() int {
+	i := 1
+	i++
+	i--
+	return i
+}
+
+func bitShiftOperator() int {
+	i := 1
+	i <<= 1
+	i >>= 1
+	return i
+}
+
+func conditionalExpression() int {
+	i := 1
+	if i > 2 {
+		i = 10
+	} else {
+		i = 20
+	}
+	return i
+}
+
+func switchStatement() int {
+	i := 1
+	switch i {
+	case 1:
+		return 10
+	case 2:
+		return 20
+	default:
+		return 30
+	}
+}
+
+func forLoop() int {
+	i := 0
+	for i < 10 {
+		i++
+	}
+	return i
+}
+
+func whileLoop() int {
+	i := 0
+	for i < 10 {
+		i++
+	}
+	return i
+}
+
+func doWhileLoop() int {
+	i := 0
+	for {
+		if i >= 10 {
+			break
+		}
+		i++
+	}
+	return i
+}
+
+func mapFunction() map[string]int {
+	m := make(map[string]int)
+	m["one"] = 1
+	m["two"] = 2
+	m["three"] = 3
+	return m
+}
+
+func sliceFunction() []int {
+	s := make([]int, 3)
+	s[0] = 1
+	s[1] = 2
+	s[2] = 3
+	return s
+}
+
+func structFunction() struct {
+	Name string
+	Age  int
+} {
+	return struct {
+		Name string
+		Age  int
+	}{
+		Name: "John",
+		Age:  30,
+	}
+}
+
+func interfaceFunction() interface{} {
+	return "Hello"
+}
+
+func pointerFunction() *int {
+	i := 1
+	return &i
+}
+
+func channelFunction() chan int {
+	c := make(chan int)
+	c <- 1
+	return c
+}
+
+func goroutineFunction() {
+	go func() {
+		fmt.Println("Hello from goroutine")
+	}()
+}
+
+func deferFunction() {
+	defer fmt.Println("Hello from defer")
+}
+
+func panicFunction() {
+	panic("Hello from panic")
+}
+
+func recoverFunction() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	panic("Hello from panic")
+}
+
+func contextFunction() {
+	ctx := context.Background()
+	fmt.Println(ctx)
+}
+
+func selectFunction() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	select {
+	case v1 := <-ch1:
+		fmt.Println(v1)
+	case v2 := <-ch2:
+		fmt.Println(v2)
+	}
+}
+
+func interfaceMethod() interface {
+	Greet() string
+}
+
+func structMethod() struct {
+	Name string
+	Age  int
+}
+
+func goroutineCall() {
+	goroutineFunction()
+}
+
+func deferCall() {
+	deferFunction()
+}
+
+func panicCall() {
+	panicFunction()
+}
+
+func recoverCall() {
+	recoverFunction()
+}
+
+func contextCall() {
+	contextFunction()
+}
+
+func selectCall() {
+	selectFunction()
+}
+
+func interfaceCall() interfaceMethod {
+	return structMethod
+}
+
+func main() {
+	main()
+}
+```
